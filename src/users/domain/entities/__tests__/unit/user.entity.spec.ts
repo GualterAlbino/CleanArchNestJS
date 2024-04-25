facfd8bc214287ab41bf1d60d00f29adc8d64e20
Gualter Albino <gualteralbino1000@gmail.com>
2024-04-25 20:50:33 -0300

    Criação de testes unitários para o constructor da entidade User

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,44 @@
+import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+
+// SUT = System Under Test (Sistema/Classe/Função em teste)
+describe('UserEntity Unit Tests', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+
+  // Hooks - Executado antes de cada teste
+  beforeEach(() => {
+    props = UserDataBuilder({});
+    sut = new UserEntity(props);
+  });
+
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Name getter', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Email getter', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('Password getter', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toEqual(props.password);
+    expect(typeof sut.props.password).toBe('string');
+  });
+
+  it('CreatedAt getter', () => {
+    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
